e0d13750aa45d57a0e20198495c9ea2af10dcac9
Stephen Paul <sylvesterpaulstephen1234@gmail.com>
2024-10-04 17:04:21 +0100

    improved the style to pixel perfect design

---
+++ index.js
@@ -0,0 +1,14 @@
+// const sidebarOpener = document.querySelector('#side-bar');
+// const sidebar = document.getElementById('sidebar-elements');
+// let toggle = false;
+
+// sidebarOpener.addEventListener('click', () => {
+//   if (toggle === false) {
+//     toggle = true;
+//   } else {
+//     toggle = false;
+//   }
+//   sidebarOpener.classList.toggle('rotate')
+
+//   sidebar.style.display = toggle ? 'flex' : 'none'
+// });
